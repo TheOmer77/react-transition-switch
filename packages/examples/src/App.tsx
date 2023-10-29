ed1a660efd85948d9553ab221baacf3fe2c349a7
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-29 18:05:27 +0200

    Add TailwindCSS + Prettier plugin to examples

--- packages/examples/src/App.tsx
+++ packages/examples/src/App.tsx
@@ -2,8 +2,11 @@ import { HELLO_WORLD } from '@theomer77/react-transition-switch';
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
+    <div
+      className='flex min-h-screen w-full flex-col items-center justify-center
+bg-white text-slate-900 dark:bg-slate-950 dark:text-slate-100'
+    >
+      <h1 className='mb-4 text-5xl font-bold'>App</h1>
       {HELLO_WORLD}
     </div>
   );
