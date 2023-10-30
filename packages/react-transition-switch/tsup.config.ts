bfaea0ad9049e61d6652f327140b8b596e86357f
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-30 16:09:23 +0200

    Setup successful build script for package

--- packages/react-transition-switch/tsup.config.ts
+++ packages/react-transition-switch/tsup.config.ts
@@ -1,15 +1,23 @@
 import { defineConfig } from 'tsup';
 
-const config = defineConfig({
-  entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
-  external: ['react', 'react-dom'],
-  sourcemap: 'inline',
-  clean: true,
-  dts: true,
-  minify: true,
-  splitting: false,
-  treeshake: true,
-});
+const config = defineConfig([
+  {
+    entry: ['src/index.ts'],
+    format: ['cjs', 'esm'],
+    external: ['react', 'react-dom'],
+    sourcemap: 'inline',
+    clean: true,
+    dts: false,
+    minify: true,
+    splitting: false,
+    treeshake: true,
+  },
+  // Workaround for dts composite error
+  // https://github.com/egoist/tsup/issues/571#issuecomment-1146858670
+  {
+    entry: { index: 'dts/index.d.ts' },
+    dts: { only: true },
+  },
+]);
 
 export default config;
