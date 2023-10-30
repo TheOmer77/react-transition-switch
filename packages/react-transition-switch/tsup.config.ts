0c628d2601c99ecb7fc64b411161b5db03d1ac91
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-30 13:42:42 +0200

    Update tsup config for react package

--- packages/react-transition-switch/tsup.config.ts
+++ packages/react-transition-switch/tsup.config.ts
@@ -3,8 +3,13 @@ import { defineConfig } from 'tsup';
 const config = defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
+  external: ['react', 'react-dom'],
+  sourcemap: 'inline',
+  clean: true,
   dts: true,
   minify: true,
+  splitting: false,
+  treeshake: true,
 });
 
 export default config;
