0e1d04301b33711a994b44fb75d9c9e7d25fe60c
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-31 23:32:34 +0200

    Adapt SharedAxis example to new TransitionSwitch states

--- packages/examples/src/components/SharedAxis.tsx
+++ packages/examples/src/components/SharedAxis.tsx
@@ -14,11 +14,6 @@ export interface SharedAxisProps extends TransitionSwitchProps {
   axis?: Axis;
 }
 
-const getDirection = (activeIndex: number, prevIndex?: number): Direction =>
-  typeof prevIndex === 'number' && activeIndex < prevIndex
-    ? 'backward'
-    : 'forward';
-
 const SharedAxis = ({
   axis = 'x',
   activeIndex,
@@ -28,7 +23,12 @@ const SharedAxis = ({
 }: SharedAxisProps) => {
   const prevIndex = usePrevious(activeIndex);
   const direction = useMemo(
-    () => getDirection(activeIndex, prevIndex),
+    (): Direction | undefined =>
+      typeof prevIndex !== 'number' || activeIndex === prevIndex
+        ? undefined
+        : activeIndex < prevIndex
+        ? 'backward'
+        : 'forward',
     [activeIndex, prevIndex]
   );
 
@@ -39,18 +39,18 @@ const SharedAxis = ({
       className={cn(
         'relative transition-[width,height] duration-300 [&>*]:absolute',
         axis === 'y'
-          ? `data-[state=closed]:[&>*]:data-[direction=backward]:animate-sharedAxis-down-out
-data-[state=closed]:[&>*]:data-[direction=forward]:animate-sharedAxis-up-out
-data-[state=open]:[&>*]:data-[direction=backward]:animate-sharedAxis-down-in
-data-[state=open]:[&>*]:data-[direction=forward]:animate-sharedAxis-up-in`
-          : `data-[state=closed]:[&>*]:data-[direction=backward]:animate-sharedAxis-right-out
-data-[state=closed]:[&>*]:data-[direction=forward]:animate-sharedAxis-left-out
-data-[state=open]:[&>*]:data-[direction=backward]:animate-sharedAxis-right-in
-data-[state=open]:[&>*]:data-[direction=forward]:animate-sharedAxis-left-in
-rtl:data-[state=closed]:[&>*]:data-[direction=backward]:animate-sharedAxis-left-out
-rtl:data-[state=closed]:[&>*]:data-[direction=forward]:animate-sharedAxis-right-out
-rtl:data-[state=open]:[&>*]:data-[direction=backward]:animate-sharedAxis-left-in
-rtl:data-[state=open]:[&>*]:data-[direction=forward]:animate-sharedAxis-right-in`,
+          ? `data-[state=inactive]:[&>*]:data-[direction=backward]:animate-sharedAxis-down-out
+data-[state=inactive]:[&>*]:data-[direction=forward]:animate-sharedAxis-up-out
+data-[state=active]:[&>*]:data-[direction=backward]:animate-sharedAxis-down-in
+data-[state=active]:[&>*]:data-[direction=forward]:animate-sharedAxis-up-in`
+          : `data-[state=inactive]:[&>*]:data-[direction=backward]:animate-sharedAxis-right-out
+data-[state=inactive]:[&>*]:data-[direction=forward]:animate-sharedAxis-left-out
+data-[state=active]:[&>*]:data-[direction=backward]:animate-sharedAxis-right-in
+data-[state=active]:[&>*]:data-[direction=forward]:animate-sharedAxis-left-in
+rtl:data-[state=inactive]:[&>*]:data-[direction=backward]:animate-sharedAxis-left-out
+rtl:data-[state=inactive]:[&>*]:data-[direction=forward]:animate-sharedAxis-right-out
+rtl:data-[state=active]:[&>*]:data-[direction=backward]:animate-sharedAxis-left-in
+rtl:data-[state=active]:[&>*]:data-[direction=forward]:animate-sharedAxis-right-in`,
         className
       )}
       data-direction={direction}
