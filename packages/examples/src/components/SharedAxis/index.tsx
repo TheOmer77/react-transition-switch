122c497ec026aea98b2240eeff7139c42bb4701c
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-11-06 13:23:16 +0200

    Move direction logic into TransitionSwitch
    Add directional prop to TransitionSwitch;
    If true, will use the same logic previously in SharedAxis.

--- packages/examples/src/components/SharedAxis/index.tsx
+++ packages/examples/src/components/SharedAxis/index.tsx
@@ -1,19 +1,15 @@
-import { Children, useEffect, useState, type ReactElement } from 'react';
-
 import {
   TransitionSwitch,
-  type TransitionSwitchItemProps,
   type TransitionSwitchProps,
 } from '@theomer77/react-transition-switch';
-import usePrevious from 'hooks/usePrevious';
 import { cn } from 'utils/cn';
 
 import { sharedAxis } from './index.module.css';
 
 export type Axis = 'x' | 'y' | 'z';
-export type Direction = 'backward' | 'forward';
 
-export interface SharedAxisProps extends TransitionSwitchProps {
+export interface SharedAxisProps
+  extends Omit<TransitionSwitchProps, 'directional'> {
   axis?: Axis;
   fadeVariant?: boolean;
 }
@@ -21,49 +17,19 @@ export interface SharedAxisProps extends TransitionSwitchProps {
 const SharedAxis = ({
   axis = 'x',
   fadeVariant = false,
-  value,
   className,
   children,
   ...props
-}: SharedAxisProps) => {
-  const [actualValue, setActualValue] = useState(value);
-  const [direction, setDirection] = useState<Direction>();
-  const prevValue = usePrevious(actualValue);
-
-  useEffect(() => {
-    const childrenArr = Children.toArray(
-      children
-    ) as ReactElement<TransitionSwitchItemProps>[];
-
-    const valueIndex = childrenArr.findIndex(
-        child => child.props.value === value
-      ),
-      prevValueIndex = childrenArr.findIndex(
-        child => child.props.value === prevValue
-      );
-
-    setDirection(
-      valueIndex < 0 || prevValueIndex < 0
-        ? undefined
-        : valueIndex < prevValueIndex
-        ? 'backward'
-        : 'forward'
-    );
-    setActualValue(value);
-  }, [children, prevValue, value]);
-
-  return (
-    <TransitionSwitch
-      {...props}
-      value={actualValue}
-      className={cn(sharedAxis, className)}
-      data-direction={direction}
-      data-axis={axis}
-      {...(fadeVariant ? { 'data-fadevariant': true } : {})}
-    >
-      {children}
-    </TransitionSwitch>
-  );
-};
+}: SharedAxisProps) => (
+  <TransitionSwitch
+    {...props}
+    directional
+    className={cn(sharedAxis, className)}
+    data-axis={axis}
+    {...(fadeVariant ? { 'data-fadevariant': true } : {})}
+  >
+    {children}
+  </TransitionSwitch>
+);
 
 export default SharedAxis;
--- packages/react-transition-switch/src/TransitionSwitch.tsx
+++ packages/react-transition-switch/src/TransitionSwitch.tsx
@@ -1,23 +1,66 @@
 import {
+  Children,
   forwardRef,
   useEffect,
   useImperativeHandle,
   useRef,
+  useState,
   type ComponentPropsWithoutRef,
+  type ReactElement,
 } from 'react';
 
 import { TransitionSwitchProvider } from './context';
+import type { TransitionSwitchItemProps } from './TransitionSwitchItem';
 
+export type TransitionSwitchDirection = 'backward' | 'forward';
 export interface TransitionSwitchProps extends ComponentPropsWithoutRef<'div'> {
   value: string;
+  directional?: boolean;
 }
 
 export const TransitionSwitch = forwardRef<
   HTMLDivElement,
   TransitionSwitchProps
->(({ value = '', children, ...props }, ref) => {
+>(({ value = '', directional = false, children, ...props }, ref) => {
+  const [directionalValue, setDirectionalValue] = useState(value);
+  const [direction, setDirection] = useState<TransitionSwitchDirection>();
+
+  const prevValue = useRef<typeof value>();
   const innerRef = useRef<HTMLDivElement>(null);
   useImperativeHandle(ref, () => innerRef.current!, []);
+
+  useEffect(() => {
+    if (!directional) return;
+    if (
+      typeof prevValue.current === 'undefined' ||
+      prevValue.current === value
+    ) {
+      prevValue.current = value;
+      return;
+    }
+
+    const childrenArr = Children.toArray(
+      children
+    ) as ReactElement<TransitionSwitchItemProps>[];
+
+    const valueIndex = childrenArr.findIndex(
+        child => child.props.value === value
+      ),
+      prevValueIndex = childrenArr.findIndex(
+        child => child.props.value === prevValue.current
+      );
+
+    setDirection(
+      valueIndex < 0 || prevValueIndex < 0
+        ? undefined
+        : valueIndex < prevValueIndex
+        ? 'backward'
+        : 'forward'
+    );
+    setDirectionalValue(value);
+
+    prevValue.current = value;
+  }, [children, directional, value]);
 
   useEffect(() => {
     const initialChild = innerRef.current?.children?.[0];
@@ -29,8 +72,17 @@ export const TransitionSwitch = forwardRef<
   }, []);
 
   return (
-    <TransitionSwitchProvider value={{ value, containerEl: innerRef.current }}>
-      <div {...props} ref={innerRef}>
+    <TransitionSwitchProvider
+      value={{
+        value: directional ? directionalValue : value,
+        containerEl: innerRef.current,
+      }}
+    >
+      <div
+        {...props}
+        {...(directional ? { 'data-direction': direction } : {})}
+        ref={innerRef}
+      >
         {children}
       </div>
     </TransitionSwitchProvider>
