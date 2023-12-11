a57ac069e5946a4f0969f3e4cfa21c5a795e288c
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-11 21:35:28 +0200

    Add theme colors

--- packages/examples/tailwind.config.ts
+++ packages/examples/tailwind.config.ts
@@ -6,6 +6,33 @@ const config: Config = {
     extend: {
       fontFamily: { sans: ['var(--font-family)', 'sans-serif'] },
     },
+    colors: {
+      inherit: 'inherit',
+      white: '#fff',
+      black: '#000',
+      transparent: 'transparent',
+      ...['primary', 'neutral', 'danger', 'secondary'].reduce(
+        (obj, colorName) => ({
+          ...obj,
+          [colorName]: [
+            50,
+            ...[...Array(9).keys()].map(key => (key + 1) * 100),
+            950,
+          ].reduce(
+            (obj, shade) => ({
+              ...obj,
+              [shade]: `rgb(var(--color-${colorName}-${shade}) / <alpha-value>)`,
+              main: `rgb(var(--color-${colorName}-main) / <alpha-value>)`,
+              light: `rgb(var(--color-${colorName}-light) / <alpha-value>)`,
+              dark: `rgb(var(--color-${colorName}-dark) / <alpha-value>)`,
+              contrast: `rgb(var(--color-${colorName}-contrast) / <alpha-value>)`,
+            }),
+            {}
+          ),
+        }),
+        {}
+      ),
+    },
   },
   plugins: [],
 };
