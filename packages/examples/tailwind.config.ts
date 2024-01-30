8d9bedf41ea9536abd9f12c18e15dd543edffd04
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-01-30 15:27:50 +0200

    Add `state-layer` tailwind utils

--- packages/examples/tailwind.config.ts
+++ packages/examples/tailwind.config.ts
@@ -1,7 +1,37 @@
 import type { Config } from 'tailwindcss';
 import animate from 'tailwindcss-animate';
+import plugin from 'tailwindcss/plugin';
+
+import flattenColorPalette from 'tailwindcss/lib/util/flattenColorPalette';
 
 const shades = [50, ...[...Array(9).keys()].map(key => (key + 1) * 100), 950];
+
+const stateLayer = plugin(({ addUtilities, matchUtilities, theme }) => {
+  const themeColors = flattenColorPalette(theme('colors'));
+
+  addUtilities({
+    '.state-layer': {
+      position: 'relative',
+      overflow: 'hidden',
+      '&.fixed': { position: 'fixed' },
+      '&.absolute': { position: 'absolute' },
+      '&::after': {
+        content: '""',
+        position: 'absolute',
+        insetBlockStart: '0',
+        insetInlineStart: '0',
+        width: '100%',
+        height: '100%',
+        zIndex: '1',
+        transition: 'background-color 75ms cubic-bezier(0.4, 0, 0.2, 1)',
+      },
+    },
+  });
+  matchUtilities(
+    { 'state-layer': value => ({ '&::after': { backgroundColor: value } }) },
+    { values: themeColors, type: 'color' }
+  );
+});
 
 const config = {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
@@ -14,8 +44,8 @@ const config = {
       },
       borderRadius: {
         lg: 'var(--border-radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        md: 'calc(var(--border-radius) - 2px)',
+        sm: 'calc(var(--border-radius) - 4px)',
       },
       fontFamily: { sans: ['var(--font-family)', 'sans-serif'] },
       keyframes: {
@@ -77,6 +107,6 @@ const config = {
       },
     },
   },
-  plugins: [animate],
+  plugins: [animate, stateLayer],
 } satisfies Config;
 export default config;
