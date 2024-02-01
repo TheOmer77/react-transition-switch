c744431de16707e5b5e7de199491af994a8e93d5
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-02-01 14:43:08 +0200

    Format Switch classnames

--- packages/examples/src/components/ui/Switch/Switch.tsx
+++ packages/examples/src/components/ui/Switch/Switch.tsx
@@ -13,7 +13,12 @@ export const Switch = forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchRoot
     className={cn(
-      'peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input',
+      `peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center
+rounded-full border-2 border-transparent shadow-sm transition-colors
+focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring
+focus-visible:ring-offset-2 focus-visible:ring-offset-background
+disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary
+data-[state=unchecked]:bg-input`,
       className
     )}
     {...props}
@@ -21,7 +26,9 @@ export const Switch = forwardRef<
   >
     <SwitchThumb
       className={cn(
-        'pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0'
+        `pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg
+ring-0 transition-transform data-[state=checked]:translate-x-4
+data-[state=unchecked]:translate-x-0`
       )}
     />
   </SwitchRoot>
