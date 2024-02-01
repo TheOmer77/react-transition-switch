5903e432df508980bf1dd95e5036d82560ec1eb6
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-02-01 18:50:55 +0200

    Add an `asChild` prop to TransitionSwitch

--- packages/react-transition-switch/src/TransitionSwitch.tsx
+++ packages/react-transition-switch/src/TransitionSwitch.tsx
@@ -8,6 +8,7 @@ import {
   type ComponentPropsWithoutRef,
   type ReactElement,
 } from 'react';
+import { Slot } from '@radix-ui/react-slot';
 
 import { TransitionSwitchProvider } from './context';
 import type { TransitionSwitchItemProps } from './TransitionSwitchItem';
@@ -22,76 +23,93 @@ export type TransitionSwitchProps = ComponentPropsWithoutRef<'div'> & {
    * which would have a value of either 'backward' or 'forward'.
    */
   directional?: boolean;
+  /**
+   * Change the default `<div>` element for the one passed as a child, merging
+   * their props and behavior.
+   *
+   * When set to `true`, this component must have a single child which accepts
+   * a ref, and its children should be `<TransitionSwitchItem>` components.
+   *
+   * @see [Radix UI Composition](https://www.radix-ui.com/primitives/docs/guides/composition)
+   */
+  asChild?: boolean;
 };
 
 export const TransitionSwitch = forwardRef<
   HTMLDivElement,
   TransitionSwitchProps
->(({ value = '', directional = false, children, ...props }, ref) => {
-  const [directionalValue, setDirectionalValue] = useState(value);
-  const [direction, setDirection] = useState<TransitionSwitchDirection>();
+>(
+  (
+    { value = '', directional = false, asChild = false, children, ...props },
+    ref
+  ) => {
+    const [directionalValue, setDirectionalValue] = useState(value);
+    const [direction, setDirection] = useState<TransitionSwitchDirection>();
 
-  const prevValue = useRef<typeof value>();
-  const innerRef = useRef<HTMLDivElement>(null);
-  useImperativeHandle(ref, () => innerRef.current!, []);
+    const prevValue = useRef<typeof value>();
+    const innerRef = useRef<HTMLDivElement>(null);
+    useImperativeHandle(ref, () => innerRef.current!, []);
 
-  useEffect(() => {
-    if (!directional) return;
-    if (
-      typeof prevValue.current === 'undefined' ||
-      prevValue.current === value
-    ) {
-      prevValue.current = value;
-      return;
-    }
+    useEffect(() => {
+      if (!directional) return;
+      if (
+        typeof prevValue.current === 'undefined' ||
+        prevValue.current === value
+      ) {
+        prevValue.current = value;
+        return;
+      }
 
-    const childrenArr = Children.toArray(
-      children
-    ) as ReactElement<TransitionSwitchItemProps>[];
+      const childrenArr = Children.toArray(
+        children
+      ) as ReactElement<TransitionSwitchItemProps>[];
 
-    const valueIndex = childrenArr.findIndex(
-        child => child.props.value === value
-      ),
-      prevValueIndex = childrenArr.findIndex(
-        child => child.props.value === prevValue.current
+      const valueIndex = childrenArr.findIndex(
+          child => child.props.value === value
+        ),
+        prevValueIndex = childrenArr.findIndex(
+          child => child.props.value === prevValue.current
+        );
+
+      setDirection(
+        valueIndex < 0 || prevValueIndex < 0
+          ? undefined
+          : valueIndex < prevValueIndex
+            ? 'backward'
+            : 'forward'
       );
+      setDirectionalValue(value);
 
-    setDirection(
-      valueIndex < 0 || prevValueIndex < 0
-        ? undefined
-        : valueIndex < prevValueIndex
-          ? 'backward'
-          : 'forward'
-    );
-    setDirectionalValue(value);
+      prevValue.current = value;
+    }, [children, directional, value]);
 
-    prevValue.current = value;
-  }, [children, directional, value]);
+    useEffect(() => {
+      const initialChild = innerRef.current?.children?.[0];
+      if (!innerRef.current || typeof initialChild === 'undefined') return;
 
-  useEffect(() => {
-    const initialChild = innerRef.current?.children?.[0];
-    if (!innerRef.current || typeof initialChild === 'undefined') return;
+      const { width, height } = getComputedStyle(initialChild);
+      innerRef.current.style.width = width;
+      innerRef.current.style.height = height;
+    }, []);
 
-    const { width, height } = getComputedStyle(initialChild);
-    innerRef.current.style.width = width;
-    innerRef.current.style.height = height;
-  }, []);
+    const Comp = asChild ? Slot : 'div';
 
-  return (
-    <TransitionSwitchProvider
-      value={{
-        value: directional ? directionalValue : value,
-        containerEl: innerRef.current,
-      }}
-    >
-      <div
-        {...props}
-        {...(directional ? { 'data-direction': direction } : {})}
-        ref={innerRef}
+    return (
+      <TransitionSwitchProvider
+        value={{
+          value: directional ? directionalValue : value,
+          containerEl: innerRef.current,
+        }}
       >
-        {children}
-      </div>
-    </TransitionSwitchProvider>
-  );
-});
+        <Comp
+          {...props}
+          {...(directional ? { 'data-direction': direction } : {})}
+          ref={innerRef}
+        >
+          {children}
+        </Comp>
+      </TransitionSwitchProvider>
+    );
+  }
+);
 TransitionSwitch.displayName = 'TransitionSwitch';
