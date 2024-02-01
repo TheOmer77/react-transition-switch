a30809542d55f1db1232416a81fce91c56a8156e
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-02-01 21:26:37 +0200

    Tweak button variants AGAIN

--- packages/examples/src/components/ui/Button/variants.ts
+++ packages/examples/src/components/ui/Button/variants.ts
@@ -2,15 +2,16 @@ import { cva } from 'class-variance-authority';
 
 export const buttonVariants = cva(
   `inline-flex cursor-default items-center justify-center whitespace-nowrap
-rounded-md text-sm font-medium transition-colors state-layer
-hover:state-layer-muted/30 focus-visible:outline-none focus-visible:ring-1
-focus-visible:ring-ring active:bg-muted/30 active:duration-0
-disabled:pointer-events-none disabled:text-muted [&>*]:z-10 [&>svg]:text-base`,
+rounded-md text-sm font-medium transition-[background-color,opacity] state-layer
+hover:state-layer-foreground/10 focus-visible:outline-none focus-visible:ring-1
+focus-visible:ring-ring active:bg-muted/30 active:duration-0 disabled:opacity-50
+disabled:pointer-events-none [&>*]:z-10 [&>svg]:text-base`,
   {
     variants: {
       variant: {
         default: `bg-background shadow-sm shadow-neutral-950/20
-dark:bg-neutral-800 dark:active:bg-neutral-700 dark:disabled:bg-neutral-800/50`,
+active:bg-neutral-200 dark:bg-neutral-800 dark:active:bg-neutral-700
+dark:active:bg-neutral-700`,
         flat: '',
       },
       size: {
