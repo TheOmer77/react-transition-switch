b8ac2df1728e6ef0cf31efe621e5a6920e93cadf
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-02-02 19:19:35 +0200

    Overhaul Checkbox design & remove its label

--- packages/examples/src/components/ui/Checkbox/Checkbox.tsx
+++ packages/examples/src/components/ui/Checkbox/Checkbox.tsx
@@ -1,22 +1,48 @@
-import type { ComponentPropsWithoutRef } from 'react';
+import { forwardRef, type ComponentPropsWithoutRef } from 'react';
+import { CheckIcon } from 'lucide-react';
+
 import { cn } from '@/lib/utils';
 
-export const Checkbox = ({
-  id,
-  label,
-  className,
-  ...props
-}: Omit<ComponentPropsWithoutRef<'input'>, 'type'> & { label: string }) => (
-  <div className='flex select-none flex-row items-center gap-1'>
-    <input
-      {...props}
-      type='checkbox'
-      id={id}
-      className={cn(
-        'h-4 w-4 accent-primary-600 dark:accent-primary-300',
-        className
-      )}
-    />
-    {label && <label htmlFor={id}>{label}</label>}
-  </div>
+export type CheckboxProps = Omit<
+  ComponentPropsWithoutRef<'input'>,
+  'type' | 'value' | 'onChange'
+> & { onCheckedChange?: (checked: boolean) => void };
+
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ checked, onCheckedChange, className, style, ...props }, ref) => {
+    return (
+      <div className='relative h-5 w-5'>
+        <input
+          {...props}
+          ref={ref}
+          type='checkbox'
+          checked={checked}
+          onChange={e => onCheckedChange?.(e.target.checked)}
+          className='peer h-full w-full opacity-0'
+        />
+        <div
+          className={cn(
+            `pointer-events-none absolute start-0 top-0 flex h-full w-full
+items-center justify-center rounded-md bg-background text-lg
+text-primary-foreground shadow-sm ring-[1.5px] ring-inset ring-border
+transition-[background-color,box-shadow] duration-75 state-layer
+peer-checked:bg-primary peer-checked:ring-primary
+peer-hover:state-layer-foreground/10
+peer-checked:peer-hover:state-layer-primary-active/50 peer-active:bg-neutral-100
+peer-active:duration-0 peer-checked:peer-active:bg-primary-active
+peer-checked:peer-active:ring-primary-active dark:bg-card
+dark:peer-active:bg-neutral-800/60 [&>svg]:z-10 [&>svg]:opacity-0
+[&>svg]:transition-opacity [&>svg]:duration-75 [&>svg]:[stroke-dasharray:2em]
+[&>svg]:[stroke-dashoffset:0] peer-checked:[&>svg]:animate-checkbox-check
+peer-checked:[&>svg]:opacity-100`,
+            className
+          )}
+          style={style}
+        >
+          <CheckIcon />
+        </div>
+      </div>
+    );
+  }
 );
+Checkbox.displayName = 'Checkbox';
--- packages/examples/tailwind.config.ts
+++ packages/examples/tailwind.config.ts
@@ -41,6 +41,8 @@ const config = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'checkbox-check':
+          'checkbox-check 300ms cubic-bezier(0.2, 1, 0.4, 1) forwards',
       },
       borderRadius: {
         lg: 'var(--border-radius)',
@@ -56,6 +58,10 @@ const config = {
         'accordion-up': {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
+        },
+        'checkbox-check': {
+          from: { strokeDashoffset: '-2em' },
+          to: { strokeDashoffset: '0' },
         },
       },
       spacing: { em: '1em', inherit: 'inherit' },
