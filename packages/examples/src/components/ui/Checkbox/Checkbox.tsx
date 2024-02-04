db9e55ecbfa8dc9418a2efadc6b646fb4b338db1
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-02-04 14:54:21 +0200

    EVEN MORE CHECKBOX TINKERING

--- packages/examples/src/components/ui/Checkbox/Checkbox.tsx
+++ packages/examples/src/components/ui/Checkbox/Checkbox.tsx
@@ -20,18 +20,19 @@ export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
           onChange={e => onCheckedChange?.(e.target.checked)}
           className={cn(
             `h-5 w-5 appearance-none content-center rounded-md bg-background
-shadow-sm ring-1 ring-inset ring-border/50 ring-offset-1 ring-offset-border
+shadow-sm ring-[0.0625rem] ring-inset ring-input
 transition-[background-color,box-shadow] duration-75 state-layer
-checked:bg-primary checked:ring-primary checked:ring-offset-primary
-hover:state-layer-foreground/10 checked:hover:state-layer-primary-active/50
-active:bg-neutral-100 active:duration-0 checked:active:bg-primary-active
-checked:active:ring-primary-active checked:active:ring-offset-primary-active
-dark:bg-card dark:active:bg-neutral-800/60 [&~svg]:pointer-events-none
-[&~svg]:absolute [&~svg]:start-[0.0625rem] [&~svg]:top-[0.0625rem] [&~svg]:z-10
-[&~svg]:text-lg [&~svg]:text-primary-foreground [&~svg]:opacity-0
-[&~svg]:transition-opacity [&~svg]:duration-75 [&~svg]:[stroke-dasharray:2em]
-[&~svg]:[stroke-dashoffset:0] [&~svg]:checked:animate-checkbox-check
-[&~svg]:checked:opacity-100`,
+checked:bg-primary checked:ring-primary hover:state-layer-foreground/10
+checked:hover:state-layer-primary-active/50 focus-visible:outline
+focus-visible:outline-2 focus-visible:outline-offset-2
+focus-visible:outline-primary active:bg-neutral-100 active:ring-input-active
+active:duration-0 checked:active:bg-primary-active
+checked:active:ring-primary-active dark:bg-card dark:active:bg-neutral-800
+[&~svg]:pointer-events-none [&~svg]:absolute [&~svg]:start-[0.0625rem]
+[&~svg]:top-[0.0625rem] [&~svg]:z-10 [&~svg]:text-lg
+[&~svg]:text-primary-foreground [&~svg]:opacity-0 [&~svg]:transition-opacity
+[&~svg]:duration-75 [&~svg]:[stroke-dasharray:2em] [&~svg]:[stroke-dashoffset:0]
+[&~svg]:checked:animate-checkbox-check [&~svg]:checked:opacity-100`,
             className
           )}
         />
