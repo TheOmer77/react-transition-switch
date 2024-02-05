57a4994dd4e298c474300e78df15487320a023b6
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-02-05 11:19:05 +0200

    Use CodeInput in VerificationStep

--- packages/examples/src/components/StepsExample/VerificationStep.tsx
+++ packages/examples/src/components/StepsExample/VerificationStep.tsx
@@ -1,38 +1,48 @@
-import { forwardRef, type ElementRef } from 'react';
+import { forwardRef, useState, type ElementRef } from 'react';
 
-import type { StepProps } from './types';
 import {
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from '@/components/ui/Card';
-import { Input } from '@/components/ui/Input';
+import { CodeInput } from '@/components/ui/CodeInput';
 import { Label } from '@/components/ui/Label';
 import { StepFooter } from './StepFooter';
+import type { StepProps } from './types';
+
+const CODE_LENGTH = 6;
 
 export const VerificationStep = forwardRef<ElementRef<'section'>, StepProps>(
-  ({ onNextClick, onPrevClick, ...props }, ref) => (
-    <section {...props} ref={ref}>
-      <CardHeader>
-        <CardTitle>Just to be safe...</CardTitle>
-        <CardDescription>
-          We&apos;ve sent a verification code to your email - please enter it
-          below.
-        </CardDescription>
-      </CardHeader>
-      <CardContent className='space-y-4'>
-        <div className='space-y-2'>
+  ({ onNextClick, onPrevClick, ...props }, ref) => {
+    const [code, setCode] = useState('');
+
+    return (
+      <section {...props} ref={ref}>
+        <CardHeader>
+          <CardTitle>Just to be safe...</CardTitle>
+          <CardDescription>
+            We&apos;ve sent a verification code to your email - please enter it
+            below.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className='space-y-4'>
           <Label htmlFor='code'>Verification Code</Label>
-          <Input id='code' placeholder='Enter your code' required />
-        </div>
-      </CardContent>
-      <StepFooter
-        onNextClick={onNextClick}
-        onPrevClick={onPrevClick}
-        nextLabel='Verify'
-      />
-    </section>
-  )
+          <CodeInput
+            value={code}
+            onChange={setCode}
+            length={CODE_LENGTH}
+            inputType='number'
+          />
+        </CardContent>
+        <StepFooter
+          onNextClick={onNextClick}
+          onPrevClick={onPrevClick}
+          nextLabel='Verify'
+          nextDisabled={code.length < CODE_LENGTH}
+        />
+      </section>
+    );
+  }
 );
 VerificationStep.displayName = 'VerificationStep';
