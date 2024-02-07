0f209710b67b34eb3fff98d1231856d76074174c
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-02-07 16:16:40 +0200

    Tweak buttons focus ring

--- packages/examples/src/components/ui/Button/variants.ts
+++ packages/examples/src/components/ui/Button/variants.ts
@@ -3,9 +3,9 @@ import { cva } from 'class-variance-authority';
 export const buttonVariants = cva(
   `inline-flex cursor-default items-center justify-center whitespace-nowrap
 rounded-md text-sm font-medium shadow-sm transition-[background-color,opacity]
-state-layer hover:state-layer-foreground/5 focus-visible:outline-none focus-visible:ring-1
-focus-visible:ring-ring active:duration-0 disabled:pointer-events-none
-disabled:opacity-50 [&>*]:z-10 [&>svg]:text-base`,
+state-layer hover:state-layer-foreground/5 focus-visible:outline-none
+focus-visible:ring-2 focus-visible:ring-ring active:duration-0
+disabled:pointer-events-none disabled:opacity-50 [&>*]:z-10 [&>svg]:text-base`,
   {
     variants: {
       variant: {
