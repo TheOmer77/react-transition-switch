a11e9a8a730013ebbfd8a06ccf7a44cecf00b16c
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-31 14:36:45 +0300

    Add `postcss-mixins`

--- packages/examples/postcss.config.js
+++ packages/examples/postcss.config.js
@@ -1,8 +1,12 @@
-export default {
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: {
+    'postcss-mixins': {},
     tailwindcss: {},
     'postcss-hover-media-feature': {},
     'postcss-viewport-unit-fallback': {},
     autoprefixer: {},
   },
 };
+
+export default config;
