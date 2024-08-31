5adbcbafbad943ed43e63bdf52dfde5bee49d6ab
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-31 13:40:08 +0300

    Update error page

--- packages/examples/src/routes/Error.tsx
+++ packages/examples/src/routes/Error.tsx
@@ -1,7 +1,7 @@
 import { useRouteError, type ErrorResponse } from 'react-router-dom';
 
 const errorMessages = {
-  DEFAULT: 'Oh no, an error occurred.',
+  DEFAULT: 'Well, that was unexpected.',
   404: "I'm not sure what you were looking for, but it's not here.",
 } as const;
 type NonDefaultErrorMessage = keyof Omit<typeof errorMessages, 'DEFAULT'>;
@@ -12,7 +12,7 @@ const ErrorPage = () => {
   return (
     <div className='flex min-h-dvh flex-col items-center justify-center'>
       <h1 className='m-0 text-9xl font-extrabold leading-none tracking-tight text-danger-800 sm:text-[16rem] md:text-[12rem] lg:text-[16rem] dark:text-danger-200'>
-        {status}
+        {status || 'Error'}
       </h1>
       <p className='m-0 text-center text-xl text-neutral-700 dark:text-neutral-300'>
         {errorMessages[status as NonDefaultErrorMessage] ||
