bb0301bb56a5afd66628990048497c1ea1b2a859
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-31 16:38:05 +0300

    Update Input in examples

--- packages/examples/src/components/ui/Input/Input.tsx
+++ packages/examples/src/components/ui/Input/Input.tsx
@@ -5,17 +5,15 @@ import { cn } from '@/lib/utils';
 export const Input = forwardRef<
   HTMLInputElement,
   ComponentPropsWithoutRef<'input'>
->(({ className, type, ...props }, ref) => {
-  return (
-    <input
-      {...props}
-      ref={ref}
-      type={type}
-      className={cn(
-        `flex h-9 w-full rounded-md border border-input bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50`,
-        className
-      )}
-    />
-  );
-});
+>(({ className, type, ...props }, ref) => (
+  <input
+    type={type}
+    className={cn(
+      'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm shadow-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+      className
+    )}
+    ref={ref}
+    {...props}
+  />
+));
 Input.displayName = 'Input';
