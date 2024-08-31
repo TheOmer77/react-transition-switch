d39c2609f6742bcb55e5b816aca50e17732abcd9
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-31 15:36:46 +0300

    Update & reformat button variants

--- packages/examples/src/components/ui/Button/variants.ts
+++ packages/examples/src/components/ui/Button/variants.ts
@@ -1,21 +1,15 @@
 import { cva } from 'class-variance-authority';
 
 export const buttonVariants = cva(
-  `inline-flex cursor-default items-center justify-center whitespace-nowrap
-rounded-md text-sm font-medium transition-[background-color,opacity]
-state-layer hover:state-layer-foreground/5 focus-visible:outline-none
-focus-visible:ring-2 focus-visible:ring-ring active:duration-0
-disabled:pointer-events-none disabled:opacity-50 [&>*]:z-10 [&>svg]:text-base
-[&>svg]:shrink-0`,
+  'inline-flex cursor-default items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-[background-color,opacity] state-layer focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring active:duration-0 disabled:pointer-events-none disabled:opacity-50 [&>*]:z-10 [&>svg]:shrink-0 [&>svg]:text-base',
   {
     variants: {
       variant: {
-        default: `border border-input/50 bg-background shadow-sm
-shadow-neutral-950/10 active:bg-neutral-100 dark:bg-neutral-800
-dark:active:bg-neutral-700`,
-        primary: `bg-primary text-primary-foreground shadow-sm shadow-primary/30
-hover:state-layer-primary-foreground/10 active:bg-primary-active`,
-        flat: 'bg-transparent active:bg-foreground/10',
+        default:
+          'border border-input/50 bg-background shadow-sm shadow-neutral-950/10 hover:state-layer-foreground/5 active:state-layer-foreground/10 dark:bg-neutral-800',
+        primary:
+          'bg-primary text-primary-foreground shadow-sm shadow-primary/30 hover:state-layer-primary-foreground/10 active:state-layer-primary-foreground/25',
+        flat: 'bg-transparent hover:state-layer-foreground/5 active:state-layer-foreground/10',
       },
       size: {
         sm: 'h-9 px-3',
